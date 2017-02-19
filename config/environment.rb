925a3b5d3af6263c0820fc4c821dcdf409d355cc
luigi <007.lva@gmail.com>
2017-02-19 18:13:39 +0100

    Fix database.yml for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,13 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#If no database configuration exists, copy the default SQLite one over...
+unless FileTest.exists?('config/database.yml')
+  FileUtils.cp 'config/database.yml.example', 'config/database.yml'
+end
+
 # Initialize the rails application
 LibreTPV::Application.initialize!
 
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
-
