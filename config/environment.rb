ff645ce8ba8b0aeb54ab3aeb593f1170e325fe76
luigi <007.lva@gmail.com>
2017-02-20 01:47:36 +0100

    Fix database.yml

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-#If no database configuration exists, copy the default SQLite one over...
-unless FileTest.exists?('config/database.yml')
-  FileUtils.cp 'config/database.yml.example', 'config/database.yml'
-end
+# If no database configuration exists, copy the default SQLite one over...
+# unless FileTest.exists?('config/database.yml')
+#   FileUtils.cp 'config/database.yml.example', 'config/database.yml'
+# end
 
 # Initialize the rails application
 LibreTPV::Application.initialize!
