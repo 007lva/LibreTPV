a7879c726e8b67bb4fb36353a0f097ab33ad1ecf
luigi <007.lva@gmail.com>
2017-04-23 01:22:56 +0200

    Update routes format

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 LibreTPV::Application.routes.draw do
-  root :to => 'albarans#index', :seccion => "caja", :controller => "albarans", :action => "index"
-  # map.connect ':seccion/:controller/:action/:id'
-  match ':seccion/:controller(/:action(/:id))'
-  # map.root :seccion => "caja", :controller => "albarans"
+  root to: 'albarans#index', seccion: 'caja', controller: 'albarans', action: 'index'
+  match ':seccion/:controller(/:action(/:id))', via: [:get, :post]
 end
